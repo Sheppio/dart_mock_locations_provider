865fcd5fb339094ee7164eb4eaa6e4328e4e5673
shep <mark_sheppard_1977@hotmail.com>
2022-05-24 21:16:20 +0100

    tidying upcode comments and dead code

--- bin/dart_mock_locations_provider.dart
+++ bin/dart_mock_locations_provider.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:gpx/gpx.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
@@ -36,56 +35,6 @@ void main(List<String> arguments) {
       updateDurationSeconds: 1,
       speed: 10);
   ml3.start();
-
-  // create gpx from xml string
-  // var xmlGpx = GpxReader().fromString(GpxRoutes.RusheyChalfontLoop);
-
-  // var trkPts = xmlGpx.trks[0].trksegs[0].trkpts;
-  // trkPts.add(trkPts[0]);
-
-  // var path = Path.from(
-  //     trkPts.map((e) => LatLng(e.lat as double, e.lon as double)).toList());
-  // var distance = path.distance;
-  // var equalizedPoints = path.equalize(2.0);
-
-  // Make route have less points if need...
-  //equalizedPoints = Path.from(equalizedPoints.coordinates.take(3));
-
-  // equalizedPoints.coordinates.forEach(((element) {
-  //   print(element);
-  // }));
-  // print("Count: ${equalizedPoints.coordinates.length}");
-  // print(distance);
-
-  // int index = 0;
-
-  // final channel = WebSocketChannel.connect(
-  //   Uri.parse('ws://127.0.0.1:8080'),
-  // );
-
-  // channel.sink.add('Hello!');
-
-  // Timer.periodic(Duration(seconds: 2), ((timer) {
-  //   var pt = equalizedPoints.coordinates[index];
-  //   //var latlng = LatLng(pt.latitude, pt.longitude);
-  //   //print(latlng.toJson());
-  //   //print(jsonEncode(latlng));
-  //   var lr = LocationReport(
-  //     timestamp: DateTime.now(),
-  //     id: "Runner_1",
-  //     latitude: pt.latitude,
-  //     longitude: pt.longitude,
-  //     // elevation: 0,
-  //     accuracy: 3.0,
-  //     // heading: 0,
-  //     // speed: 1.0,
-  //   );
-  //   var messageToServer = "LOCATION_REPORT#${jsonEncode(lr)}";
-  //   print(messageToServer);
-  //   channel.sink.add(messageToServer);
-  //   index++;
-  //   index = index % equalizedPoints.coordinates.length;
-  // }));
 }
 
 class MockLocation {
